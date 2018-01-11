1af253c0443f39f8dffc5167509e86bc63450ccd
Andrew Friedman <andrewfriedman01@gmail.com>
2018-01-10 21:53:32 -0500

    Added App
    First version of Acronym Creator GitHub has ever seen!

---
+++ Assets/Script/MainMenu.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+var names : UnityEngine.UI.InputField;
+function Create () {
+	PlayerPrefs.SetString("Name", names.text);
+	Application.LoadLevel("Create");
+}
+
+function QuitGame () {
+	Debug.Log ("Game is exiting...");
+	Application.Quit ();
+}
